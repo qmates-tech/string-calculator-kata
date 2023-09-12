2d81d8022509fd99921895ec35c30d846e561fe5
Daniele Megna <megna.dany@gmail.com>
2023-09-12 15:59:08 +0200

    Simplify foo sample test

--- tests/foo.spec.ts
+++ tests/foo.spec.ts
@@ -1,10 +1,6 @@
 import 'jest-extended';
-import { mock } from 'jest-mock-extended';
 
-describe('Foo', () => {
-
-  test('jest is working', () => {
-    expect(true).toBeTrue()
-  })
-
+test('jest is working', () => {
+  expect(true).toBeTrue()
 })
+
